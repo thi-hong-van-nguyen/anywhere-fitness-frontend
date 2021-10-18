84a607f39f4af66dbdb6750b012cfbb12adf349c
thi <vannguyen141290@gmail.com>
2021-10-17 19:03:52 -0700

    wip

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        hello
+        header
       </header>
+      <main>
+        main
+      </main>
+      <footer>
+        footer
+      </footer>
     </div>
   );
 }
